65b15f591a215773a5205dc1cb1d489759002a76
harri <harrybalanta@gmail.com>
2024-11-02 08:16:20 -0500

    se agrega a la tarea la parte opcional utiliznado otros depencias

--- CLASE-2/Harrinson-Tarea-2/index.js
+++ CLASE-2/Harrinson-Tarea-2/index.js
@@ -16,5 +16,29 @@ console.log("Correo electrónico:", correo);
 console.log("Fecha de nacimiento:", fechaNacimiento.toLocaleDateString());
 console.log("Teléfono:", telefono);
 
+// utilizando dependencias
+const twitter = chance.twitter();
+// impresion
 
+console.log("utilizando twitter",twitter )
 
+// usage
+const naturales1 =chance.natural();
+const naturales2 = chance.natural({ min: 1, max: 20 });
+// impresion
+console.log("impresion numeros naturales", naturales1,);
+console.log("impresion numeros naturales", naturales2,);
+
+// cc
+const cc1 = chance.cc();
+const cc2 = chance.cc({type: 'Mastercard'});
+// impresion
+console.log("impresion cc", cc1);
+console.log("impresion cc", cc2);
+// animal
+const animal1 = chance.animal();
+const animal2 = chance.animal({type: 'zoo'});
+
+// impresion
+console.log("impresion de animal1", animal1);
+console.log("impresion  de animal2", animal2);
